ea3b950e59380bc15bcb67814d87b4db98e9c746
u3585 <gimgt@naver.com>
2026-05-13 12:31:44 +0900

    Add files via upload

---
+++ stonenwater/data/exhibition.js
@@ -0,0 +1,62 @@
+
+Json{
+  "menu": ["current exhibition", "upcoming exhibition", "past exhibition"],
+  "exhibitions": [
+    {
+      "id": 1,
+      "title": "리빙퍼니처(LIVING FURNITURE) #1",
+      "category": "current exhibition",
+      "startDate": "2002-06-16",
+      "endDate": "2002-08-16",
+      "thumbnail": "images/exhibit1.jpg",
+      "description": "보충대리공간 스톤앤워터 개관전"
+    },
+    {
+      "id": 2,
+      "title": "재건축 프로젝트 2002 - 서울에서 가장 살기 좋은 잠실동, 안양의 명당 석수동 #2",
+      "category": "current exhibition",
+      "startDate": "2002-11-17",
+      "endDate": "2002-12-01",
+      "thumbnail": "images/exhibit2.jpg",
+      "description": "서울 잠실동과 안양 석수동 연계전시"
+    },
+    {
+      "id": 3,
+      "title": "새로운 희망(NEW HOPE) #1",
+      "category": "upcoming exhibition",
+      "startDate": "2003-02-15",
+      "endDate": "2003-03-15",
+      "thumbnail": "images/exhibit3.jpg",
+      "description": "예술장터"
+    },
+    {
+      "id": 4,
+      "title": "상상도서관 #2",
+      "category": "upcoming exhibition",
+      "startDate": "2003-05-03",
+      "endDate": "2003-05-30",
+      "thumbnail": "images/exhibit4.jpg",
+      "description": "마을도서관 리모델링 및 예술가들이 만든 도서관의 모습들."
+    },
+    {
+      "id": 5,
+      "title": "김석용 <들꽃항아리> #1",
+      "category": "past exhibition",
+      "startDate": "2003-06-07",
+      "endDate": "2003-06-21",
+      "thumbnail": "images/exhibit5.jpg",
+      "description": "도예가 김석용이 만든 항아리와 들꽃"
+    },
+    {
+      "id": 6,
+      "title": "웰컴투 작업실 <여기서 놀자> #2",
+      "category": "past exhibition",
+      "startDate": "2025-03-10",
+      "endDate": "2025-04-30",
+      "thumbnail": "images/exhibit6.jpg",
+      "description": "노는 것 같이 작업하는 예술가들의 모습들"
+    }
+    
+  ]
+}
+
