debad032e5fdc4cb1af2a0794ab8613c83c6f978
u3585 <gimgt@naver.com>
2026-05-13 12:52:36 +0900

    Update main.js

--- stonenwater/data/main.js
+++ stonenwater/data/main.js
@@ -1,1 +1,17 @@
+window.onload = () => {
+ 		fetch("exhibition.json")
+            .then((response) => response.json())
+            .then((json) => {
+                data = json.list;
+                
+                const jsonListWrap = document.getElementById("jsonListWrap");
 
+                data.forEach(el => {
+                    jsonListWrap.innerHTML += `
+                        <li> id : ${el.number}</li>
+                        <li> title : ${el.name}</li>
+                        <li> ${e1.starDate} - ${e1.endData}</li>
+                    `;
+                });
+        });
+}
